524557929d0051c71dccbf682826967179bf77a4
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-07-11 17:25:42 -0300

    Criando as entidades

---
+++ src/main/java/com/example/api/domain/address/Address.java
@@ -0,0 +1,23 @@
+package com.example.api.domain.address;
+
+import com.example.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
