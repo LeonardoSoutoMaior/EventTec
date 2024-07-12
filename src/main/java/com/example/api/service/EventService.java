eee417da152da56edb64b06a0742978b43cb05df
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-07-12 11:34:39 -0300

    criação do controller e service do event

---
+++ src/main/java/com/example/api/service/EventService.java
@@ -0,0 +1,41 @@
+package com.example.api.service;
+
+import com.example.api.domain.event.Event;
+import com.example.api.domain.event.EventRequestDTO;
+import com.example.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    public Event createEvent(EventRequestDTO data){
+        String imgUrl = null;
+
+        if(data.image() != null){
+            imgUrl = this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        repository.save(newEvent);
+
+        return newEvent;
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
+        return "";
+    }
+}
