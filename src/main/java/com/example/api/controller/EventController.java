80b7ae1c40c44885e31fad83cf24c806d901f1a8
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-07-12 14:53:31 -0300

    Listando os eventos com paginação

--- src/main/java/com/example/api/controller/EventController.java
+++ src/main/java/com/example/api/controller/EventController.java
@@ -2,13 +2,13 @@ package com.example.api.controller;
 
 import com.example.api.domain.event.Event;
 import com.example.api.domain.event.EventRequestDTO;
+import com.example.api.domain.event.EventResponseDTO;
 import com.example.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -21,5 +21,11 @@ public class EventController {
     public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
         Event newEvent = this.eventService.createEvent(body);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        List<EventResponseDTO> allEvents = this.eventService.getEvents(page,size);
+        return ResponseEntity.ok(allEvents);
     }
 }
--- src/main/java/com/example/api/service/EventService.java
+++ src/main/java/com/example/api/service/EventService.java
@@ -2,12 +2,17 @@ package com.example.api.service;
 
 import com.example.api.domain.event.Event;
 import com.example.api.domain.event.EventRequestDTO;
+import com.example.api.domain.event.EventResponseDTO;
 import com.example.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class EventService {
@@ -33,6 +38,14 @@ public class EventService {
         repository.save(newEvent);
 
         return newEvent;
+    }
+
+
+    public List<EventResponseDTO> getEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findAll(pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.getRemote(), event.getEventUrl() ,event.getImgUrl()))
+                .stream().toList();
     }
 
     private String uploadImg(MultipartFile multipartFile){
