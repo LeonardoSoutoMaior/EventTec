7b41fd6509da42631cb0323060c2473b54615aaa
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-07-12 12:07:05 -0300

    2° alteração no bd

--- src/main/resources/db/migration/V2__create-table-coupon.sql
+++ src/main/resources/db/migration/V2__create-table-coupon.sql
@@ -1,6 +1,7 @@
 CREATE TABLE coupon(
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     code VARCHAR(100) NOT NULL,
+    discount INTEGER NOT NULL,
     valid TIMESTAMP NOT NULL,
     event_id UUID,
     FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
